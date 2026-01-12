7e50efc8c30388efc7b071f373d9ec53f7ae59f8
Daniel Brockhaus <daniel@brockhaus.org>
2026-01-12 09:22:06 +0000

    Fixed problems with version.c; creation of .obj/; correct URL for repo.

--- readme.md
+++ readme.md
@@ -13,7 +13,7 @@ To run under Ubuntu 22.04.4 LTS (tested and working as of 2026.01.06)
 ```
 sudo apt-get update
 sudo apt install git
-git clone https://github.com/DanielBrockhaus/astonia_server.git
+git clone https://github.com/AstoniaCommunity/astonia_community_server35
 sudo apt-get install gcc-multilib
 sudo dpkg --add-architecture i386
 sudo apt install make
--- version.c
+++ version.c
@@ -2,19 +2,33 @@
  * Part of Astonia Server 3.5 (c) Daniel Brockhaus. Please read license.txt.
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-result"
 
 int main(void) {
     int handle, val;
     char buf[256];
 
     handle = open("vers.c", O_RDWR);
-    read(handle, buf, 250);
-    val = atoi(buf + 13);
+    if (handle == -1) {
+        handle = open("vers.c", O_WRONLY | O_CREAT, 0644);
+        val = (3 << 24) + (5 << 16);
+    } else {
+        read(handle, buf, 250);
+        val = atoi(buf + 13);
+        lseek(handle, 0, 0);
+    }
+
     sprintf(buf, "int VERSION = %d;\nchar *VER_DATE = __DATE__;\nchar *VER_TIME = __TIME__;\n", val + 1);
-    lseek(handle, 0, 0);
     write(handle, buf, strlen(buf));
     close(handle);
     return 0;
 }
+
+#pragma GCC diagnostic pop
